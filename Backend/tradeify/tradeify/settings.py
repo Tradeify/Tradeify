582377a6113beffc7b9349399a40bc871295458d
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-12-21 15:36:47 -0600

    Add Djongo

--- Backend/tradeify/tradeify/settings.py
+++ Backend/tradeify/tradeify/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tradeify',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'tradeify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'tradeify',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://otheradmin:qwerty123@tradeify.nr06g.mongodb.net/test?authSource=admin&replicaSet=atlas-1rj3dt-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
+        }
     }
 }
 
