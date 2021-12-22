0c6e7922d2b96e08180ad3d07d215acc525bd890
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-12-22 13:34:36 -0600

    Add models for profile and tradenotes

--- Backend/tradeify/api/models.py
+++ Backend/tradeify/api/models.py
@@ -6,22 +6,24 @@ class Profile(models.Model):
     username = models.CharField(max_length=100)
     full_name = models.CharField(max_length=200)
 
-
     def __str__(self) -> str:
         return f"\nUsername: {self.username}\nFull Name: {self.full_name}"
 
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = (
-            'username', 'full_name'
-        )
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = (
+#             'username', 'full_name'
+#         )
 
 
 class Kpi(models.Model):
     ticker = models.CharField(max_length=10)
     value = models.FloatField()
+
+    class Meta:
+        abstract = True
 
     def __str__(self) -> str:
         return f"\nTicker: {self.ticker} \nValue: {self.value}"
@@ -42,6 +44,8 @@ class Trade(models.Model):
     entry_price = models.FloatField()
     exit_price = models.FloatField()
 
+    class Meta:
+        abstract = True
 
     def __str__(self) -> str:
         return f"\nTicker: {self.ticker}"
@@ -59,6 +63,9 @@ class Media(models.Model):
     media_type = models.CharField(max_length=100)
     media_name = models.CharField(max_length=100)
     media_data = models.CharField(max_length=10000)
+
+    class Meta:
+        abstract = True
 
     def __str__(self) -> str:
         return f"\nMedia: {self.media_name}"
@@ -87,13 +94,15 @@ class Tradenotes(models.Model):
                              model_form_class=KpiForm)
     trades = models.ArrayField(model_container=Trade,
                                model_form_class=TradeForm)
+    objects = models.DjongoManager()
 
 
-class TradenotesForm(forms.ModelForm):
-    class Meta:
-        model = Tradenotes
-        fields = (
-            'summary', 'rationale', 'begin_time', 'end_price', 'last_modified', 'last_modefied_date', 'created_date', 'emotions', 'media', 'kpis', 'trades'
-        )
-    def __str__(self) -> str:
-        return f"\nTradenotes: {self.summary}"
+# class TradenotesForm(forms.ModelForm):
+#     class Meta:
+#         model = Tradenotes
+#         fields = (
+#             'summary', 'rationale', 'begin_time', 'end_price', 'last_modified', 'last_modefied_date', 'created_date', 'emotions', 'media', 'kpis', 'trades'
+#         )
+
+#     def __str__(self) -> str:
+#         return f"\nTradenotes: {self.summary}"
