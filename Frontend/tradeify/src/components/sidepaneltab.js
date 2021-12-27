b029db834ba4d06a3395e878091780fa6eee3e80
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2021-12-27 15:48:37 -0500

    Update sidepaneltab.js

--- Frontend/tradeify/src/components/sidepaneltab.js
+++ Frontend/tradeify/src/components/sidepaneltab.js
@@ -8,69 +8,90 @@ import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 import SearchIcon from '@mui/icons-material/Search';
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
 export default function SidePanelTab() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%', color: 'black' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant='fullWidth'
-          TabIndicatorProps={{
-            style: {
-              background: 'black'
-            }
-          }}
-          textColor="inherit">
-          <Tab icon={<div className='flex flex-row items-center capitalize'><InsertDriveFileIcon />Notes</div>} {...a11yProps(0)} />
-          <Tab icon={<div className='flex flex-row items-center capitalize'><SearchIcon />Search</div>} {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%', color: 'black' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant='fullWidth'
+                    TabIndicatorProps={{
+                        style: {
+                            background: 'black'
+                        }
+                    }}
+                    textColor="inherit">
+                    <Tab icon={<div className='flex flex-row items-center capitalize'><InsertDriveFileIcon />Notes</div>} {...a11yProps(0)} />
+                    <Tab icon={<div className='flex flex-row items-center capitalize'><SearchIcon />Search</div>} {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                <SidePanelItem spitemtitle={'All Tradenotes'} spitemsubtitle={'Add, Edit, Preview'}
+                    className={'bg-purple-900 text-white'}
+                ><InsertDriveFileIcon /></SidePanelItem>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+        </Box>
+    );
 }
 
+class SidePanelItem extends React.Component {
+    render() {
+        return (
+            <div className='flex flex-col'>
+                <div className='flex flex-row px-2 py-3 mb-3 mt-5'>
+                    <div className={"p-2 rounded " + this.props.className}>
+                        {this.props.children}
+                    </div>
+                    <div className='flex flex-col justify-center ml-3'>
+                        <div className='text-sm text-black font-semibold'>{this.props.spitemtitle}</div>
+                        <div className='text-sm mt-[2px] text-slate-400 font-semibold'>{this.props.spitemsubtitle}</div>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
