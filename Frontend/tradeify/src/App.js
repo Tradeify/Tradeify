2ef81dbacaa272896bad83d556dc1292248777ee
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2021-12-27 10:17:58 -0500

    front

--- Frontend/tradeify/src/App.js
+++ Frontend/tradeify/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Sidebar from './components/sidebar';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row">
+      <Sidebar></Sidebar>
+      <h1>Hello World</h1>
     </div>
   );
 }
