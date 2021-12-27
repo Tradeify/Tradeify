33229764e8e88cbd242a93c596ffe1d06d440161
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-12-27 14:56:25 -0600

    remove padding

--- Frontend/tradeify/src/components/sidebar.js
+++ Frontend/tradeify/src/components/sidebar.js
@@ -9,9 +9,6 @@ class Sidebar extends React.Component {
             <div className='sidebar flex flex-col outline-slate-400 border-r-2'>
                 <ProfileSection name={'Omotunde Yusuff'} initials={'OY'}></ProfileSection>
                 <SidePanelTab></SidePanelTab>
-                <SidePanelItem spitemtitle={'All Tradenotes'} spitemsubtitle={'Add, Edit, Preview'}
-                className={'bg-purple-900 text-white'}
-                ><InsertDriveFileIcon/></SidePanelItem>
             </div>
         )
     }
@@ -33,27 +30,5 @@ class ProfileSection extends React.Component {
         );
     }
 }
-
-class SidePanelItem extends React.Component {
-    render() {
-        return (
-            <div className='flex flex-col'>
-                <div className='flex flex-row px-2 py-3 mb-3 mt-5'>
-                    <div className={"p-2 rounded " + this.props.className}>
-                        {this.props.children}
-                    </div>
-                    <div className='flex flex-col justify-center ml-3'>
-                        <div className='text-sm text-black font-semibold'>{this.props.spitemtitle}</div>
-                        <div className='text-sm mt-[2px] text-slate-400 font-semibold'>{this.props.spitemsubtitle}</div>
-                        <div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-
 
 export default Sidebar;
