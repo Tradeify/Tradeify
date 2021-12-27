94d462281d1fd196b0f95d500cedcbe68de41a72
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-12-27 14:20:19 -0600

    Update sidepaneltab.js

--- Frontend/tradeify/src/components/sidepaneltab.js
+++ Frontend/tradeify/src/components/sidepaneltab.js
@@ -1,10 +1,47 @@
-import * as React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import SearchIcon from '@mui/icons-material/Search';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function SidePanelTab() {
-  const [value, setValue] = React.useState('one');
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -12,18 +49,27 @@ export default function SidePanelTab() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-      >
-        <Tab value="one" label="Item One">
-
-        </Tab>
-        <Tab value="two" label="Item Two" />
-      </Tabs>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%', color: 'black' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant='fullWidth'
+          TabIndicatorProps={{
+            style: {
+              background: 'black'
+            }
+          }}
+          textColor="inherit">
+          <Tab icon={<div className='flex flex-row items-center capitalize'><InsertDriveFileIcon />Notes</div>} {...a11yProps(0)} />
+          <Tab icon={<div className='flex flex-row items-center capitalize'><SearchIcon />Search</div>} {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
     </Box>
   );
 }
