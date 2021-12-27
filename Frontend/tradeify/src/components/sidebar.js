337c1a9db57060e8d48c4d760f412962a68c12a7
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2021-12-27 11:55:37 -0500

    sidebar

--- Frontend/tradeify/src/components/sidebar.js
+++ Frontend/tradeify/src/components/sidebar.js
@@ -1,13 +1,28 @@
 import React from "react";
-
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
 class Sidebar extends React.Component {
     render() {
         return (
             <div className='sidebar flex flex-col'>
-                This is sidebar
+                <ProfileSection name={'Omotunde Yusuff'} initials={'OY'}></ProfileSection>
             </div>
         )
     }
 }
+
+class ProfileSection extends React.Component {
+    render() {
+        return (
+            <div className='flex flex-row topSection'>
+                <a href='/'><img src="logo.png"/></a>
+                <div className="initialsLogo"><p>{this.props.initials}</p></div>
+                <div className='flex flex-row'><p>{this.props.name}</p></div>
+                <button><ChevronLeftIcon/></button>
+            </div>
+        );
+    }
+}
+
 
 export default Sidebar;
