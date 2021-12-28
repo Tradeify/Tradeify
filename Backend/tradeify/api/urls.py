fd8b114a8f45b8b4160466ad93cf94a6287ced41
Tobi Adegunloye <Oloruntobiadg@gmail.com>
2021-12-28 17:34:59 -0600

    Add Unauthorized for login

--- Backend/tradeify/api/urls.py
+++ Backend/tradeify/api/urls.py
@@ -6,6 +6,7 @@ from . import views, tradenotes_functions
 urlpatterns = [
         path('create_user', views.Create_User, name='create_user'),
         path('login_user', views.Login_User, name='login_user'),
+        path('send_unauthorized', views.Send_Unauthorized, name='send_unauthorized'),
         path('create_tradenote', tradenotes_functions.create_Tradenote, name='create_tradenote'),
         path('get_tradenote/<int:tradenote_id>', tradenotes_functions.get_Tradenote, name='get_tradenote'),
         path("get_all_tradenotes", tradenotes_functions.get_All_Tradenotes, name="get_all_tradenotes"),
