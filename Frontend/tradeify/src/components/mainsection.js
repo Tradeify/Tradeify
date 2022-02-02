4ba4b5df3a77476719e28852ffd68782ea8f7b36
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2022-02-01 21:36:57 -0500

    Update fetch api + state issue

--- Frontend/tradeify/src/components/mainsection.js
+++ Frontend/tradeify/src/components/mainsection.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function MainSection(props) {
-   const [alltradenotes, setAllTradenotes] = React.useState([])
+var alltradenotes = []
 
    function GetAllTradenotes() {
       var requestOptions = {
@@ -25,8 +25,8 @@ function MainSection(props) {
 
          )
          .then(result => {
-            console.log(result)
-            setAllTradenotes(result.Tradenotes)
+            alltradenotes = result.Tradenotes
+            console.log(alltradenotes)
          })
          .catch(error => {
             console.log('error', error)
@@ -34,38 +34,18 @@ function MainSection(props) {
    }
 
    if (props.performRequest) {
-      GetAllTradenotes();
+      GetAllTradenotes(); 
    }
 
    return (
       <div className="p-4 mainSection">
 
          {alltradenotes.map((tradenote) => {
-            return (<TradeNote />)
+            console.log(tradenote)
+            return (<TradeNote summary={tradenote.summary}/>)
          })}
 
-         <TradeNote />
-         <TradeNote summary={`
-What is Lorem Ipsum?
-
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-Why do we use it?
-
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-Where does it come from?
-
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites`} />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
-         <TradeNote />
+     
       </div>
    );
 }
